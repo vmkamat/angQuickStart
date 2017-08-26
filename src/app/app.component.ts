1dd705f3c00d66991eeaf97b19849ddeb7175277
vinay kamat <vmkamat@gmail.com>
2017-08-26 12:16:53 -0400

    created app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  /*  template: `` */
+  templateUrl: 'app/app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  title = 'My Simple App'
+  name = 'Vinay!';
+  textColor = 'blue';
+
+  changeSuitColor(){
+    this.textColor = this.textColor === 'blue'? 'red' : 'blue';
+  }
+
+}
